f5d3c05c61322263e900f157a48323886a59ba42
UmiKami <ggoku02@gmail.com>
2021-12-25 19:56:07 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let excuse = document.querySelector(".main-phrase")
+
+let who = ['The dog','My aunt','An alien','My cat', 'A rat', ''];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
